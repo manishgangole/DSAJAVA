b315a41a5e46bf47436b6934a695648212b6fe21
Manish Gangole <131508521+manishgangole@users.noreply.github.com>
2026-02-19 13:54:39 +0530

    Add Largest number finder program
    Implement a program to find the largest of three numbers.

--- Largest.java
+++ Largest.java
@@ -1,4 +1,21 @@
 package PACKAGE_NAME;
 
 public class Largest {
+  public static void main(String[] args) {
+        //Q find largest number
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        int max = a;
+        if (b > max){
+            max = b;
+        }
+        if (c > max){
+            max =c;
+        }
+        System.out.println(max);
+    }
 }
